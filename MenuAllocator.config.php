5cf4ddc024709ee80630b29a601771bbb6ec1fee
dtnjgl <account@datajungle.xyz>
2023-09-04 22:09:40 +0100

    more explanation on config page

--- MenuAllocator.config.php
+++ MenuAllocator.config.php
@@ -5,7 +5,8 @@ $config = array(
     'ma_menus' => array(
         'label' => 'Menus Options',
         'type' => 'Text',
-        'description' => 'Define the available menus of your website. Those menus will render as checkboxes on every page under /Home/.  You can access a page\'s selected menu with $page->meta->get(\'ma_menus\')'
+        'description' => 'Enter all available menus of your website separated by an empty space. These menus will render as checkboxes on every page under /Home/.',
+        'placeholder' => 'e.g. top footer …'
     ),
 );
 
